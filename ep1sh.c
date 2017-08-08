c2bbc8c08569e0d3dc8b2d333629bdf87dd642ce
viagostini <vinicius.agostini19@gmail.com>
2017-08-07 22:59:59 -0300

    shell com chown

---
+++ ep1sh.c
@@ -0,0 +1,75 @@
+#include "ep1sh.h"
+
+#define PATH_SIZE 500
+#define MAX_ARG_NUM 5
+
+char path[PATH_SIZE];
+
+char *builtin[] = {"chown", "date", "exit"};
+
+int main (int argc, char **argv) {
+    char prompt[PATH_SIZE+4];
+    char *input, **input_tokens;
+
+    getcwd(path, sizeof(path));
+    using_history();
+
+    while (1) {
+        sprintf(prompt, "[%s]$ ", path);
+        input = readline(prompt);
+        add_history(input);
+        input_tokens = tokenize(input);
+        free(input);
+        if (is_builtin(input_tokens[0]))
+            run_builtin(input_tokens);
+    }
+
+    return 0;
+}
+
+/* =======================================
+ *
+ *              FUNÇÔES
+ *
+ * =======================================
+ */
+
+char **tokenize (char *str) {
+    int i = 0;
+    char *token, **args;
+
+    token = strtok(str, " ");
+    args = (char**) malloc(MAX_ARG_NUM * sizeof(char*) + 1);
+
+    while (token != NULL) {
+        int len = strlen(token);
+        args[i] = (char *) malloc(len * sizeof(char));
+        strcpy(args[i], token);
+        token = strtok(NULL, " ");
+        i++;
+    }
+
+    args[i] = NULL;
+    return args;
+}
+
+void run_builtin (char **input_tokens) {
+    struct group* newgroup = getgrnam(input_tokens[1]);
+    if(strcmp(input_tokens[0], "chown") == 0) {
+        int ch = chown(input_tokens[2], -1, newgroup->gr_gid);
+        return;
+    }
+
+    if(strcmp(input_tokens[0], "exit") == 0)
+        exit(EXIT_SUCCESS);
+}
+
+int is_builtin (char *cmd) {
+    int i;
+    int cmd_num = sizeof(builtin) / sizeof(builtin[0]);
+    
+    for (i = 0; i < cmd_num; i++)
+        if (strcmp(cmd, builtin[i]) == 0)
+            return 1;
+    return 0;
+}
---
+++ ep1sh.h
@@ -0,0 +1,41 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <grp.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+/*
+ * A função tokenize() recebe uma string str e a particiona em uma
+ * array de strings args.
+ */
+
+char **tokenize (char *str);
+
+/*
+ * A função runBuiltIn() recebe a array de strings input_tokens e
+ * decide qual comando embutido deve ser rodado pelo shell.
+ */
+
+void run_builtin (char **input_tokens);
+
+/*
+ * A função runExternal() recebe a array de strings input_tokens e
+ * decide qual comando embutido deve ser rodado pelo shell.
+ */
+
+void runExternal (char **input_tokens);
+
+/*
+ * A função isBuiltIn() recebe a string cmd e retorna true caso
+ * a string identifique um comando embutido e false caso contrário.
+ */
+
+int is_builtin (char *cmd);
+
+#endif  /* SHELL_H_ */
