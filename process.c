b9ae3dc0b98a987e14c292026bc1c2a6f7e2a2ca
Vinicius <vinicius.agostini19@gmail.com>
2017-08-14 15:22:45 -0300

    estrutura basica de um processo e uns espaços no timer.c

---
+++ process.c
@@ -0,0 +1,6 @@
+typedef struct process {
+    char name[100];
+    float t0;
+    float dt;
+    float deadline;
+} Process;
--- timer.c
+++ timer.c
@@ -2,21 +2,21 @@
 
 struct timespec timer_start() {
     struct timespec clock;
-    clock_gettime(CLOCK_MONOTONIC, &clock);
     
+    clock_gettime(CLOCK_MONOTONIC, &clock);
     return clock;
 }
 
 float timer_elapsed(struct timespec start, struct timespec end) {
     float f_start = (float) start.tv_sec + (1e-9 * start.tv_nsec);
     float f_end = (float) end.tv_sec + (1e-9 * end.tv_nsec);
-    
+
     return f_end - f_start;
 }
 
 float timer_check(struct timespec timer) {
     struct timespec curr;
-    clock_gettime(CLOCK_MONOTONIC, &curr);
     
+    clock_gettime(CLOCK_MONOTONIC, &curr);
     return timer_elapsed(curr, timer);
 }
