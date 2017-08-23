6a872a48127d0ce0cc5c8da102e1202b55161d97
Marcelo Trylesinski <marcelotryle@outlook.com>
2017-08-23 19:27:38 -0300

    Função compareProcess() adicionada.

--- process.c
+++ process.c
@@ -1,12 +1,34 @@
 #include "process.h"
 
+Process create_process(float t0, float dt, float deadline, char* name);
+int compareProcess(Process p1, Process p2);
+
 Process create_process(float t0, float dt, float deadline, char* name) {
     Process new_process = malloc(sizeof (Process*));
-    
+
     new_process->t0 = t0;
     new_process->dt = dt;
     new_process->deadline = deadline;
     strcpy(new_process->name, name);
 
     return new_process;
+}
+
+int compareProcess(Process p1, Process p2) {
+    if (p1->t0 < p2->t0)
+        return 1;
+    else if (p1->t0 > p2->t0)
+        return -1;
+    else {
+        if (p1->dt < p2->dt)
+            return 1;
+        else if (p1->dt > p2->dt)
+            return -1;
+        else {
+            if (p1->deadline <= p2->deadline)
+                return 1;
+            else
+                return -1;
+        }
+    }
 }
--- process.h
+++ process.h
@@ -13,4 +13,6 @@ typedef struct process *Process;
 
 Process create_process(float t0, float dt, float deadline, char* name);
 
+int compareProcess(Process p1, Process p2);
+
 #endif /* PROCESS_H */
