deff796f3476f9dce210a92c5f4245393c27a5c8
Kludex <marcelo.usp@outlook.com>
2017-09-01 11:47:39 -0300

    Função estática adicionada.

--- timer.c
+++ timer.c
@@ -1,7 +1,10 @@
 #include "timer.h"
 
+float timer_check(struct timespec init);
+static float timer_elapsed(struct timespec start, struct timespec end);
+
 /* A função timer_elapsed() devolve um float que representa o tempo que passou
-   entre dois instantes de tempo, representados por timespecs, em segundos. */
+// entre dois instantes de tempo, representados por timespecs, em segundos. */
 static float timer_elapsed(struct timespec start, struct timespec end) {
     float f_start = (float) start.tv_sec + (1e-9 * start.tv_nsec);
     float f_end = (float) end.tv_sec + (1e-9 * end.tv_nsec);
--- timer.h
+++ timer.h
@@ -3,10 +3,6 @@
 
 #include <time.h>
 
-/* A função timer_elapsed() devolve um float que representa o tempo que passou
-   entre dois instantes de tempo, representados por timespecs, em segundos. */
-float timer_elapsed(struct timespec start, struct timespec end);
-
 /* A função timer_check() devolve um float que representa o tempo que passou em
    relação à um instante de tempo inicial init */
 float timer_check(struct timespec init);
