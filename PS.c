5f8ce530741ef450cd1c6562fabb6b42c8f72e22
Marcelo Trylesinski <marcelotryle@outlook.com>
2017-09-06 13:25:02 -0300

    Função que calcula o tempo que daremos a cada processo no PS feita.

--- PS.c
+++ PS.c
@@ -1,12 +1,14 @@
 #include "PS.h"
 
 void PS(Process *v, int size);
+float time_ps(float deadline);
 
 void PS(Process *v, int size) {
     int i = 0;
-    float timestamp;
+    float timestamp, quantum_time;
     struct timespec init, now;
     Heap h;
+    Process prev = NULL;
 
     h = create_heap();
     clock_gettime(CLOCK_MONOTONIC, &init);
@@ -18,9 +20,31 @@ void PS(Process *v, int size) {
             insert_process(h, v[i++]);
 
         Process p = get_min(h);
+
+        if(prev != NULL)
+            context++;
         if (p != NULL) {
-            printf("Rodando processo [%s] por %f segundos\n", p->name, p->dt);
-            run_process(p->dt);
+            quantum_time = time_ps(p->deadline);
+            printf("Rodando processo [%s] por %f segundos\n", p->name, quantum_time >= p->dt ? p->dt : quantum_time);
+            if (quantum_time >= p->dt) {
+                run_process(p->dt);
+                print_process(p, init);
+            } else {
+                run_process(quantum_time);
+                p->dt -= quantum_time;
+
+                timestamp = timer_check(init);
+
+                while (i < size && v[i]->t0 <= timestamp)
+                    insert_process(h, v[i++]);
+
+                insert_process(h, p);
+            }
         }
+        prev = p;
     }
+}
+
+float time_ps(float deadline) {
+   return (1.0 / deadline) * QUANTUM;
 }
--- PS.h
+++ PS.h
@@ -7,9 +7,12 @@
 #include "timer.h" /* timer_check() */
 #include "heap.h" /* insert_process(), get_min(), create_heap() */
 #include "process.h" /* Process */
+#include "ep.h" /* context */
 
 /* A função PS() executa o algoritmo de escalonamento com prioridade com os
 // processos v[0..size-1]. */
 void PS(Process *v, int size);
+
+float time_ps(float deadline);
 
 #endif /* PS_H */
