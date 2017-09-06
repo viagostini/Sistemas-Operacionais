5a75daa17ecc7ef2bbb51b4c78f68aa5c772ee3e
Marcelo Trylesinski <marcelotryle@outlook.com>
2017-09-06 13:24:20 -0300

    Debug retirado.

--- SJF.c
+++ SJF.c
@@ -15,15 +15,12 @@ void SJF(Process *v, int size) {
     while(h->size > 0 || i < size) {
         timestamp = timer_check(init);
 
-        while (i < size && v[i]->t0 <= timestamp) {
-            printf("%s INSERIDO\n", v[i]->name);
+        while (i < size && v[i]->t0 <= timestamp)
             insert_process(h, v[i++]);
-        }
 
         Process p = get_min(h);
-        printf("%s PEGO\n", p->name);
 
-        if(prev != NULL && !process_equal(p, prev))
+        if(prev != NULL)
             context++;
         if (p != NULL) {
             printf("Rodando processo [%s] por %f segundos\n", p->name, p->dt);
