ec5bfe631648940cc80a21bcbb40cb815120b1d7
viagostini <vinicius.agostini19@gmail.com>
2017-09-09 19:50:42 -0300

    script de teste atualizado

--- runTests.sh
+++ runTests.sh
@@ -4,7 +4,8 @@
 # Roda ep com o escalonador 2 para os arquivos 1,2,3,4,5.
 
 SCHED=$1
-TEST=$2
+BEGIN=$2
+END=$3
 
 echo '\nTestando escalonador' $SCHED '\n'
 
@@ -20,14 +21,15 @@ if [ ! -d "debug" ]; then
     mkdir debug
 fi
 
-for T in `seq 1 $TEST`; do
+for T in `seq $BEGIN $END`; do
     #trace_file=Testes/teste$T.txt
     trace_file=traces/trace_file_$T.txt
-    result_file=results/result_$SCHED.$T.txt
+    result_file=results/result_$SCHED.$T
 
     echo '\nRodando teste' $T 'com algoritmo' $SCHED'!\n'
 
-    ./ep $SCHED $trace_file $result_file d 2> debug/debug_$SCHED.$T || echo $erro.$SCHED.$T
+    #2> debug/debug_$SCHED.$T
+    ./ep $SCHED $trace_file $result_file d || echo $erro.$SCHED.$T
 
 done
 
