b8b92f36f44d2ae4a5edd36228532544b48ed4a7
viagostini <vinicius.agostini19@gmail.com>
2017-10-29 04:30:58 -0200

    bloco de memoria usado na implementação com lista ligada

---
+++ EP3/MemBlock.java
@@ -0,0 +1,34 @@
+
+
+public class MemBlock {
+    private int base;
+    private int limit;
+    private int size;
+
+    public MemBlock (int base, int limit) {
+        this.base = base;
+        this.limit = limit;
+        size = limit - base + 1;
+    }
+
+    public int getBase() {
+        return this.base;
+    }
+
+    public int getLimit() {
+        return this.limit;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void setAddress (int base, int limit) {
+        this.base = base;
+        this.limit = limit;
+    }
+
+    public boolean full() {
+        return this.base > this.limit;
+    }
+}
