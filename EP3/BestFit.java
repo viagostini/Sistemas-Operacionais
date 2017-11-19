129a057410c70dce97af17cbcdf0a1ac94036e2c
viagostini <vinicius.agostini19@gmail.com>
2017-11-19 19:14:44 -0200

    correção erro bobo

--- EP3/BestFit.java
+++ EP3/BestFit.java
@@ -37,7 +37,7 @@ public class BestFit extends FreeSpaceManager {
         // junta blocos quebrados que se tornaram contíguos eventualmente
         joinBlocks(usedBlocks, i-1);
 
-        memBlock.setAddress(memBlock.getBase() + p.size(), memBlock.getLimit());
+        freeBlocks.setAddress(memBlock.getBase() + p.size(), memBlock.getLimit());
         if (freeBlocks.get(idx).full())
             freeBlocks.remove(idx);
 
