8dac1b4fd5d038620fc9b8ceb00b66898b42b85d
viagostini <vinicius.agostini19@gmail.com>
2017-11-19 22:50:35 -0200

    fifo em progresso

---
+++ EP3/FIFO.java
@@ -0,0 +1,34 @@
+public class FIFO extends PagingManager {
+    int last = -1;
+    Page[] pages;
+
+    public FIFO (int virtual, int p, int physical) {
+        super(virtual, p, physical);
+        pages = new Page[n_pages];
+    }
+
+    public int findFreePage () {
+        if (last == pages.length-1)
+            return -1;
+
+        return ++last;
+    }
+
+    public void addPageFrame (Page p) {
+        if (last == n_pages - 1)
+            removePageFrame();
+
+        pages[idx] = p;
+        p.setPosPhysical(idx);
+        } else {
+            removePage();
+            pages
+        }
+    }
+
+    public Page removePageFrame() {
+        last--;
+        return pages.remove(0);
+    }
+
+}
